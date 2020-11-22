0d03973bb4d5ff5771d47f90f9d2b2450a37f25a
James Foster <github@jgfoster.net>
2020-11-22 10:46:38 -0800

    Save WIP.

--- scripts/install_libraries.sh
+++ scripts/install_libraries.sh
@@ -2,19 +2,24 @@
 
 # set up directories
 bundle exec ensure_arduino_installation.rb
+cd $(bundle exec arduino_library_location.rb)
 
 # get custom version of LiquidCrystal
-git clone https://github.com/Arduino-CI/LiquidCrystal.git
-mv LiquidCrystal $(bundle exec arduino_library_location.rb)
+if [ ! -d LiquidCrystal ]; then
+  git clone https://github.com/Arduino-CI/LiquidCrystal.git
+fi
 
 # get custom version of RTClib
-git clone https://github.com/Arduino-CI/RTClib.git
-mv RTClib $(bundle exec arduino_library_location.rb)
+if [ ! -d RTClib ]; then
+  git clone https://github.com/Arduino-CI/RTClib.git
+fi
 
 # get custom version of Adafruit_BusIO
-git clone https://github.com/Arduino-CI/Adafruit_BusIO.git
-mv Adafruit_BusIO $(bundle exec arduino_library_location.rb)
+if [ ! -d Adafruit_BusIO ]; then
+  git clone https://github.com/Arduino-CI/Adafruit_BusIO.git
+fi
 
 # get custom version of Adafruit_MAX31865
-git clone https://github.com/Arduino-CI/Adafruit_MAX31865.git
-mv Adafruit_MAX31865 $(bundle exec arduino_library_location.rb)
+if [ ! -d Adafruit_MAX31865 ]; then
+  git clone https://github.com/Arduino-CI/Adafruit_MAX31865.git
+fi
